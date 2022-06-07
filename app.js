fb25002c7313b3b6eb95641cad6cc6d7d7a6adfe
Liidia Laada <liidia.laada@gmail.com>
2022-06-07 19:49:43 +0300

    Add register page

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const mustacheExpress = require('mustache-express');
 const PORT = 4000;
+
+app.engine('mustache', mustacheExpress());
+app.set('views','./views');
+app.set('view engine', 'mustache');
+
+app.get('/register', (request,response) => {
+  response.render('register');
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
